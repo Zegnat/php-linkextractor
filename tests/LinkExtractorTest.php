04cff1ed8c854126218a7b1974cf0a6fb9e91948
Martijn van der Ven <martijn@vanderven.se>
2017-11-17 17:01:45 +0100

    Skeleton material
    Stealing some ideas from https://github.com/thephpleague/skeleton/.

---
+++ tests/LinkExtractorTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Testing if LinkExtractor extracts links.
+ *
+ * PHP version 7
+ *
+ * @author    Martijn van der Ven <martijn@vanderven.se>
+ * @copyright 2017 Martijn van der Ven
+ * @license   BSD Zero Clause License
+ * @version   0.1.0
+ * @link      https://github.com/Zegnat/php-linkextractor
+ */
+
+declare(strict_types=1);
+
+namespace Zegnat\LinkExtractor;
+
+class LinkExtractorTest extends \PHPUnit\Framework\TestCase
+{
+
+}
